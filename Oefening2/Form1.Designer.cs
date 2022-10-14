c4a34c53ef2d1276397374c3fe3b0f4e162337ed
Billy-San <bilal.bakkali@student.ehb.be>
2022-10-14 19:16:57 +0200

    VS3

---
+++ Oefening2/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Oefening2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stapelint = new System.Windows.Forms.Label();
+            this.StapelIntBox = new System.Windows.Forms.TextBox();
+            this.StapelintToevoegen = new System.Windows.Forms.Button();
+            this.StapelIntVerwijderen = new System.Windows.Forms.Button();
+            this.LeegmakenStapelint = new System.Windows.Forms.Button();
+            this.ShowListStapelInt = new System.Windows.Forms.Button();
+            this.SearchStapelInt = new System.Windows.Forms.Button();
+            this.CopyStapelInt = new System.Windows.Forms.Button();
+            this.CopyStapelVanString = new System.Windows.Forms.Button();
+            this.SearchStapelVanString = new System.Windows.Forms.Button();
+            this.ShowListStapelVanString = new System.Windows.Forms.Button();
+            this.LeegMakenStapelVanString = new System.Windows.Forms.Button();
+            this.VerwijderenStapelVanString = new System.Windows.Forms.Button();
+            this.toevoegenStapelVanString = new System.Windows.Forms.Button();
+            this.StapelVanStringBox = new System.Windows.Forms.TextBox();
+            this.StapelVanString = new System.Windows.Forms.Label();
+            this.MeubelCopy = new System.Windows.Forms.Button();
+            this.MeubelSearch = new System.Windows.Forms.Button();
+            this.MeubelShowList = new System.Windows.Forms.Button();
+            this.MeubelLeegmaken = new System.Windows.Forms.Button();
+            this.MeubelVerwijderen = new System.Windows.Forms.Button();
+            this.MeubelToevoegen = new System.Windows.Forms.Button();
+            this.MeubelBox = new System.Windows.Forms.TextBox();
+            this.TypeMeubel = new System.Windows.Forms.Label();
+            this.Prijs = new System.Windows.Forms.Label();
+            this.PrijsBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Stapelint
+            // 
+            this.Stapelint.AutoSize = true;
+            this.Stapelint.Location = new System.Drawing.Point(92, 25);
+            this.Stapelint.Name = "Stapelint";
+            this.Stapelint.Size = new System.Drawing.Size(85, 25);
+            this.Stapelint.TabIndex = 0;
+            this.Stapelint.Text = "Stapel int";
+            // 
+            // StapelIntBox
+            // 
+            this.StapelIntBox.Location = new System.Drawing.Point(63, 66);
+            this.StapelIntBox.Name = "StapelIntBox";
+            this.StapelIntBox.Size = new System.Drawing.Size(150, 31);
+            this.StapelIntBox.TabIndex = 1;
+            // 
+            // StapelintToevoegen
+            // 
+            this.StapelintToevoegen.Location = new System.Drawing.Point(80, 118);
+            this.StapelintToevoegen.Name = "StapelintToevoegen";
+            this.StapelintToevoegen.Size = new System.Drawing.Size(112, 34);
+            this.StapelintToevoegen.TabIndex = 2;
+            this.StapelintToevoegen.Text = "Toevoegen";
+            this.StapelintToevoegen.UseVisualStyleBackColor = true;
+            // 
+            // StapelIntVerwijderen
+            // 
+            this.StapelIntVerwijderen.Location = new System.Drawing.Point(80, 162);
+            this.StapelIntVerwijderen.Name = "StapelIntVerwijderen";
+            this.StapelIntVerwijderen.Size = new System.Drawing.Size(112, 34);
+            this.StapelIntVerwijderen.TabIndex = 3;
+            this.StapelIntVerwijderen.Text = "Verwijderen";
+            this.StapelIntVerwijderen.UseVisualStyleBackColor = true;
+            // 
+            // LeegmakenStapelint
+            // 
+            this.LeegmakenStapelint.Location = new System.Drawing.Point(80, 202);
+            this.LeegmakenStapelint.Name = "LeegmakenStapelint";
+            this.LeegmakenStapelint.Size = new System.Drawing.Size(112, 34);
+            this.LeegmakenStapelint.TabIndex = 4;
+            this.LeegmakenStapelint.Text = "Leegmaken";
+            this.LeegmakenStapelint.UseVisualStyleBackColor = true;
+            // 
+            // ShowListStapelInt
+            // 
+            this.ShowListStapelInt.Location = new System.Drawing.Point(80, 242);
+            this.ShowListStapelInt.Name = "ShowListStapelInt";
+            this.ShowListStapelInt.Size = new System.Drawing.Size(112, 34);
+            this.ShowListStapelInt.TabIndex = 5;
+            this.ShowListStapelInt.Text = "Show List";
+            this.ShowListStapelInt.UseVisualStyleBackColor = true;
+            // 
+            // SearchStapelInt
+            // 
+            this.SearchStapelInt.Location = new System.Drawing.Point(80, 282);
+            this.SearchStapelInt.Name = "SearchStapelInt";
+            this.SearchStapelInt.Size = new System.Drawing.Size(112, 34);
+            this.SearchStapelInt.TabIndex = 6;
+            this.SearchStapelInt.Text = "Search";
+            this.SearchStapelInt.UseVisualStyleBackColor = true;
+            // 
+            // CopyStapelInt
+            // 
+            this.CopyStapelInt.Location = new System.Drawing.Point(80, 322);
+            this.CopyStapelInt.Name = "CopyStapelInt";
+            this.CopyStapelInt.Size = new System.Drawing.Size(112, 34);
+            this.CopyStapelInt.TabIndex = 7;
+            this.CopyStapelInt.Text = "Copy";
+            this.CopyStapelInt.UseVisualStyleBackColor = true;
+            // 
+            // CopyStapelVanString
+            // 
+            this.CopyStapelVanString.Location = new System.Drawing.Point(284, 322);
+            this.CopyStapelVanString.Name = "CopyStapelVanString";
+            this.CopyStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.CopyStapelVanString.TabIndex = 15;
+            this.CopyStapelVanString.Text = "Copy";
+            this.CopyStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // SearchStapelVanString
+            // 
+            this.SearchStapelVanString.Location = new System.Drawing.Point(284, 282);
+            this.SearchStapelVanString.Name = "SearchStapelVanString";
+            this.SearchStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.SearchStapelVanString.TabIndex = 14;
+            this.SearchStapelVanString.Text = "Search";
+            this.SearchStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // ShowListStapelVanString
+            // 
+            this.ShowListStapelVanString.Location = new System.Drawing.Point(284, 242);
+            this.ShowListStapelVanString.Name = "ShowListStapelVanString";
+            this.ShowListStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.ShowListStapelVanString.TabIndex = 13;
+            this.ShowListStapelVanString.Text = "Show List";
+            this.ShowListStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // LeegMakenStapelVanString
+            // 
+            this.LeegMakenStapelVanString.Location = new System.Drawing.Point(284, 202);
+            this.LeegMakenStapelVanString.Name = "LeegMakenStapelVanString";
+            this.LeegMakenStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.LeegMakenStapelVanString.TabIndex = 12;
+            this.LeegMakenStapelVanString.Text = "Leegmaken";
+            this.LeegMakenStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // VerwijderenStapelVanString
+            // 
+            this.VerwijderenStapelVanString.Location = new System.Drawing.Point(284, 162);
+            this.VerwijderenStapelVanString.Name = "VerwijderenStapelVanString";
+            this.VerwijderenStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.VerwijderenStapelVanString.TabIndex = 11;
+            this.VerwijderenStapelVanString.Text = "Verwijderen";
+            this.VerwijderenStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // toevoegenStapelVanString
+            // 
+            this.toevoegenStapelVanString.Location = new System.Drawing.Point(284, 118);
+            this.toevoegenStapelVanString.Name = "toevoegenStapelVanString";
+            this.toevoegenStapelVanString.Size = new System.Drawing.Size(112, 34);
+            this.toevoegenStapelVanString.TabIndex = 10;
+            this.toevoegenStapelVanString.Text = "Toevoegen";
+            this.toevoegenStapelVanString.UseVisualStyleBackColor = true;
+            // 
+            // StapelVanStringBox
+            // 
+            this.StapelVanStringBox.Location = new System.Drawing.Point(267, 66);
+            this.StapelVanStringBox.Name = "StapelVanStringBox";
+            this.StapelVanStringBox.Size = new System.Drawing.Size(150, 31);
+            this.StapelVanStringBox.TabIndex = 9;
+            // 
+            // StapelVanString
+            // 
+            this.StapelVanString.AutoSize = true;
+            this.StapelVanString.Location = new System.Drawing.Point(267, 25);
+            this.StapelVanString.Name = "StapelVanString";
+            this.StapelVanString.Size = new System.Drawing.Size(143, 25);
+            this.StapelVanString.TabIndex = 8;
+            this.StapelVanString.Text = "Stapel van string";
+            // 
+            // MeubelCopy
+            // 
+            this.MeubelCopy.Location = new System.Drawing.Point(485, 322);
+            this.MeubelCopy.Name = "MeubelCopy";
+            this.MeubelCopy.Size = new System.Drawing.Size(112, 34);
+            this.MeubelCopy.TabIndex = 23;
+            this.MeubelCopy.Text = "Copy";
+            this.MeubelCopy.UseVisualStyleBackColor = true;
+            // 
+            // MeubelSearch
+            // 
+            this.MeubelSearch.Location = new System.Drawing.Point(485, 282);
+            this.MeubelSearch.Name = "MeubelSearch";
+            this.MeubelSearch.Size = new System.Drawing.Size(112, 34);
+            this.MeubelSearch.TabIndex = 22;
+            this.MeubelSearch.Text = "Search";
+            this.MeubelSearch.UseVisualStyleBackColor = true;
+            // 
+            // MeubelShowList
+            // 
+            this.MeubelShowList.Location = new System.Drawing.Point(485, 242);
+            this.MeubelShowList.Name = "MeubelShowList";
+            this.MeubelShowList.Size = new System.Drawing.Size(112, 34);
+            this.MeubelShowList.TabIndex = 21;
+            this.MeubelShowList.Text = "Show List";
+            this.MeubelShowList.UseVisualStyleBackColor = true;
+            // 
+            // MeubelLeegmaken
+            // 
+            this.MeubelLeegmaken.Location = new System.Drawing.Point(485, 202);
+            this.MeubelLeegmaken.Name = "MeubelLeegmaken";
+            this.MeubelLeegmaken.Size = new System.Drawing.Size(112, 34);
+            this.MeubelLeegmaken.TabIndex = 20;
+            this.MeubelLeegmaken.Text = "Leegmaken";
+            this.MeubelLeegmaken.UseVisualStyleBackColor = true;
+            // 
+            // MeubelVerwijderen
+            // 
+            this.MeubelVerwijderen.Location = new System.Drawing.Point(485, 162);
+            this.MeubelVerwijderen.Name = "MeubelVerwijderen";
+            this.MeubelVerwijderen.Size = new System.Drawing.Size(112, 34);
+            this.MeubelVerwijderen.TabIndex = 19;
+            this.MeubelVerwijderen.Text = "Verwijderen";
+            this.MeubelVerwijderen.UseVisualStyleBackColor = true;
+            // 
+            // MeubelToevoegen
+            // 
+            this.MeubelToevoegen.Location = new System.Drawing.Point(485, 118);
+            this.MeubelToevoegen.Name = "MeubelToevoegen";
+            this.MeubelToevoegen.Size = new System.Drawing.Size(112, 34);
+            this.MeubelToevoegen.TabIndex = 18;
+            this.MeubelToevoegen.Text = "Toevoegen";
+            this.MeubelToevoegen.UseVisualStyleBackColor = true;
+            // 
+            // MeubelBox
+            // 
+            this.MeubelBox.Location = new System.Drawing.Point(468, 66);
+            this.MeubelBox.Name = "MeubelBox";
+            this.MeubelBox.Size = new System.Drawing.Size(150, 31);
+            this.MeubelBox.TabIndex = 17;
+            // 
+            // TypeMeubel
+            // 
+            this.TypeMeubel.AutoSize = true;
+            this.TypeMeubel.Location = new System.Drawing.Point(482, 25);
+            this.TypeMeubel.Name = "TypeMeubel";
+            this.TypeMeubel.Size = new System.Drawing.Size(113, 25);
+            this.TypeMeubel.TabIndex = 16;
+            this.TypeMeubel.Text = "Type Meubel";
+            // 
+            // Prijs
+            // 
+            this.Prijs.AutoSize = true;
+            this.Prijs.Location = new System.Drawing.Point(717, 25);
+            this.Prijs.Name = "Prijs";
+            this.Prijs.Size = new System.Drawing.Size(44, 25);
+            this.Prijs.TabIndex = 24;
+            this.Prijs.Text = "Prijs";
+            // 
+            // PrijsBox
+            // 
+            this.PrijsBox.Location = new System.Drawing.Point(660, 66);
+            this.PrijsBox.Name = "PrijsBox";
+            this.PrijsBox.Size = new System.Drawing.Size(150, 31);
+            this.PrijsBox.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 543);
+            this.Controls.Add(this.PrijsBox);
+            this.Controls.Add(this.Prijs);
+            this.Controls.Add(this.MeubelCopy);
+            this.Controls.Add(this.MeubelSearch);
+            this.Controls.Add(this.MeubelShowList);
+            this.Controls.Add(this.MeubelLeegmaken);
+            this.Controls.Add(this.MeubelVerwijderen);
+            this.Controls.Add(this.MeubelToevoegen);
+            this.Controls.Add(this.MeubelBox);
+            this.Controls.Add(this.TypeMeubel);
+            this.Controls.Add(this.CopyStapelVanString);
+            this.Controls.Add(this.SearchStapelVanString);
+            this.Controls.Add(this.ShowListStapelVanString);
+            this.Controls.Add(this.LeegMakenStapelVanString);
+            this.Controls.Add(this.VerwijderenStapelVanString);
+            this.Controls.Add(this.toevoegenStapelVanString);
+            this.Controls.Add(this.StapelVanStringBox);
+            this.Controls.Add(this.StapelVanString);
+            this.Controls.Add(this.CopyStapelInt);
+            this.Controls.Add(this.SearchStapelInt);
+            this.Controls.Add(this.ShowListStapelInt);
+            this.Controls.Add(this.LeegmakenStapelint);
+            this.Controls.Add(this.StapelIntVerwijderen);
+            this.Controls.Add(this.StapelintToevoegen);
+            this.Controls.Add(this.StapelIntBox);
+            this.Controls.Add(this.Stapelint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label Stapelint;
+        private TextBox StapelIntBox;
+        private Button StapelintToevoegen;
+        private Button StapelIntVerwijderen;
+        private Button LeegmakenStapelint;
+        private Button ShowListStapelInt;
+        private Button SearchStapelInt;
+        private Button CopyStapelInt;
+        private Button CopyStapelVanString;
+        private Button SearchStapelVanString;
+        private Button ShowListStapelVanString;
+        private Button LeegMakenStapelVanString;
+        private Button VerwijderenStapelVanString;
+        private Button toevoegenStapelVanString;
+        private TextBox StapelVanStringBox;
+        private Label StapelVanString;
+        private Button MeubelCopy;
+        private Button MeubelSearch;
+        private Button MeubelShowList;
+        private Button MeubelLeegmaken;
+        private Button MeubelVerwijderen;
+        private Button MeubelToevoegen;
+        private TextBox MeubelBox;
+        private Label TypeMeubel;
+        private Label Prijs;
+        private TextBox PrijsBox;
+    }
+}
---
+++ Oefening2/Meubel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening2
+{
+    internal class Meubel
+    {
+        private string TypeMeubel { get; set; }
+        private int Prijs { get; set; }
+
+        public Meubel(string typeMeubel, int prijs)
+        {
+            TypeMeubel = typeMeubel;
+            Prijs = prijs;
+        }
+
+        public override string ToString()
+        {
+            return TypeMeubel + Prijs;
+        }
+    }
+}
